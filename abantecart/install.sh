f3fa4206e264ab4897d9406a0ffd9194035dad47
Andres Meseguer Rojas <amesegue@akamai.com>
2021-05-24 12:21:19 -0600

    abantecart to Apache2

--- abantecart/install.sh
+++ abantecart/install.sh
@@ -10,5 +10,5 @@ cp ./letsencrypt/etc/live/$HOSTNAME/fullchain.pem ./certs/fullchain.pem&&
 cp ./letsencrypt/etc/live/$HOSTNAME/privkey.pem ./certs/privkey.pem &&
 docker run  --network abantecart --name abantecart \
   --restart always  -p 80:80 -p 443:443 \
-  -v "$PWD/certs":/etc/ssl/letsencrypt \
+  -v "$PWD/certs":/etc/ssl/apache2 \
   ameseguer/abantecart
